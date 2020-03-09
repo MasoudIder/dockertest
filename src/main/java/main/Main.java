14be86c4a6a890272b4b73642e7b16e0a448d2ef
administrator <masoudider@yahoo.com>
2020-03-09 16:22:02 +0330

    2020.1

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        for (int i=0;i<100;i++){
+            System.out.println("Hello Boy");
+            Thread.sleep(1000);
+        }
+    }
+}
